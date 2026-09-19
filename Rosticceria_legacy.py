97859ffe6ecc3716cb7b8c68243a11a1ae407c3f
Sebastiano-Mazzarisi <s.mazzarisi@mazzarisi.it>
2026-09-19 09:22:38 +0200

    Menu Michela 18/09 + aggiornato numero telefono Michela

--- Rosticceria_legacy.py
+++ Rosticceria_legacy.py
@@ -3487,7 +3487,7 @@ def write_publish_index(panels: List[Dict], output_dir: str) -> None:
         "Fantasia": "080-405.41.39",
         "Cibària": "080-645.07.99",
         "Impastamò": "392-536.15.36",
-        "Le delizie di Michela": "080-521.22.33",
+        "Le delizie di Michela": "080-521.42.33",
         "Santoro (Castellana)": "080-859.83.13",
         "Pane & Co": "080-405.49.00",
         "Bollenti piatti": "334-318.58.44",
